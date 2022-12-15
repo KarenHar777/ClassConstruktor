be61aff1377cb6aef3bff879f26f3d55bdb5bf68
Karen Harutyunyan <karenvardanovich@gmail.com>
2022-12-15 15:47:24 +0400

    Add project files.

---
+++ ClassHome1/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassHome1
+{
+    public class Address
+    {
+        public int index;
+        public string country;
+        public string city;
+        public string street;
+        public int building;
+        public int house;
+
+        
+
+        public void Method()
+        {
+            Console.WriteLine($"Index: {index} \nCountry: {country} \nCity: {city} \nStreet: {street} \nBuilding: {building} \nHouse: {house}");
+        }
+    }
+}
---
+++ ClassHome1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+
+namespace ClassHome1
+{
+    class Program
+    {
+        public static Address Karen()
+        {
+            Address karen = new Address();
+            karen.index = 0096;
+            karen.country = "Armenia";
+            karen.city = "Yerevan";
+            karen.street = "Moldovakan";
+            karen.building = 2;
+            karen.house = 1;
+
+            return karen;
+        }
+
+        static void Main(string[] args)
+        {
+            var KarenAddress = Karen();
+
+            KarenAddress.Method();
+        }
+    }
+}
